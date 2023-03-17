ae79daf013c493d55cb6209c3f209d24693ec5c8
João Vitor Delaia <115042912+M4ntov4@users.noreply.github.com>
2023-03-17 20:21:33 -0300

    Add files via upload

---
+++ testePont/Ponto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.cuzin;
+
+namespace testePont
+{
+    class Ponto
+    {
+        int posicaoX;
+        int posicaoY;
+
+        public int PosicaoY { get => posicaoY; set => posicaoY = value; }
+        public int PosicaoX { get => posicaoX; set => posicaoX = value; }
+
+        public Ponto()
+        {
+            this.PosicaoY = 0;
+            this.PosicaoX = 0;
+        }
+
+        public Ponto(Ponto Teste)
+        {
+            this.PosicaoX = Teste.posicaoX;
+            this.PosicaoY = Teste.PosicaoY;
+        }
+
+        public Ponto(int x, int y)
+        {
+            this.PosicaoX = x;
+            this.PosicaoY = y;
+        }
+
+
+    }
+}
